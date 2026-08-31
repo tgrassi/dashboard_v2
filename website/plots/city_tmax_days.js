810b6a83122ebc84033ef5b2b2465b5965d3bf0d
tgrassi <tgrass@mep.mpg.de>
2026-08-31 19:29:55 +0200

    website city

---
+++ website/plots/city_tmax_days.js
@@ -0,0 +1,3 @@
+// see test.js for an example of how to use this function
+
+plot("data/city_tmax_days.json", "city_tmax_days");
---
+++ website/plots/city_tmin_days.js
@@ -0,0 +1,3 @@
+// see test.js for an example of how to use this function
+
+plot("data/city_tmin_days.json", "city_tmin_days");
